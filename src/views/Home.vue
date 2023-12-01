f754798152d04fcb005b6bf83d758562b1ef5462
xaraxx <sara.galvan@udea.edu.co>
2023-12-01 17:22:41 -0500

    creating ui project

---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="align-center text-center fill-height">
+      <div>Hola</div>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup>
+  import HelloWorld from '@/components/HelloWorld.vue'
+</script>
